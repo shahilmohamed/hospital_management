0bb2cd15940b4ff772d69f7b482d145309743aaa
shahilmohamed <shahil.m.mohamed@gmail.com>
2025-05-13 23:35:11 +0530

    Altered mapping relationship in entity classes

--- src/main/java/com/project/hospitalReport/controller/PatientController.java
+++ src/main/java/com/project/hospitalReport/controller/PatientController.java
@@ -1,5 +1,5 @@
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+package com.project.hospitalReport.controller;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
