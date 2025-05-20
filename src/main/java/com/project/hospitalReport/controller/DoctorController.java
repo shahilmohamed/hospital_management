b02c27a020d39b59daa5612055b6f72f885846b7
shahilmohamed <shahil.m.mohamed@gmail.com>
2025-05-20 23:58:41 +0530

    Signup dao is done

--- src/main/java/com/project/hospitalReport/controller/DoctorController.java
+++ src/main/java/com/project/hospitalReport/controller/DoctorController.java
@@ -1,15 +1,24 @@
 package com.project.hospitalReport.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.project.hospitalReport.dao.DoctorDao;
+import com.project.hospitalReport.dto.Doctor;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200", methods = {RequestMethod.POST, RequestMethod.GET, RequestMethod.PUT, RequestMethod.DELETE})
 public class DoctorController {
+	
+	@Autowired
+	DoctorDao dao;
 	
 	@GetMapping("hello")
 	public void hello()
@@ -17,6 +26,13 @@ public class DoctorController {
 		ResponseEntity<String> res = new ResponseEntity<>(HttpStatus.OK);
 		System.out.println(res);
 		
+	}
+	
+	@PostMapping("signup")
+	public String signup(@RequestBody Doctor doctor)
+	{
+		String result = dao.signup(doctor);
+		return result;
 	}
 	
 
