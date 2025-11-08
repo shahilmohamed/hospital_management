77d02ced67a272b5774494e9595b8cd9b4d84902
shahilmohamed <shahil.m.mohamed@gmail.com>
2025-11-08 18:00:22 +0530

    Added docker file

--- src/test/java/com/example/hospitalReport/HospitalReportApplicationTests.java
+++ src/test/java/com/example/hospitalReport/HospitalReportApplicationTests.java
@@ -1,9 +1,10 @@
 package com.example.hospitalReport;
 
+import com.project.hospitalReport.HospitalReportApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = HospitalReportApplication.class)
 class HospitalReportApplicationTests {
 
 	@Test
