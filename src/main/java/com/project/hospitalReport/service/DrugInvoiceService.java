0056a8f3d4aa48159b3cde9d3b241c28573a7e86
shahilmohamed <shahil.m.mohamed@gmail.com>
2026-06-22 18:11:12 +0530

    Added get all invoice API

--- src/main/java/com/project/hospitalReport/service/DrugInvoiceService.java
+++ src/main/java/com/project/hospitalReport/service/DrugInvoiceService.java
@@ -1,8 +1,12 @@
 package com.project.hospitalReport.service;
 
+import com.project.hospitalReport.dto.PageRequ;
 import com.project.hospitalReport.entity.DrugInvoice;
 import com.project.hospitalReport.repository.DrugInvoiceRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +16,11 @@ public class DrugInvoiceService {
 
     public DrugInvoice insertInvoice(DrugInvoice drugInvoice){
         return drugInvoiceRepo.save(drugInvoice);
+    }
+
+    public Page<DrugInvoice> getAllInvoice(PageRequ pageRequ){
+        Pageable pageable = PageRequest.of(pageRequ.getPage(), pageRequ.getSize());
+        return drugInvoiceRepo.findAll(pageable);
     }
 
 }
