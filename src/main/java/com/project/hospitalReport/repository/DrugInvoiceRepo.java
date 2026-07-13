09b6b48ac1b8db9d25851adfaa15be43af6cade6
mohamedshahil30 <shahil.m@tya.co.in>
2026-07-13 13:42:53 +0530

    Auto generated invoice number is added

--- src/main/java/com/project/hospitalReport/repository/DrugInvoiceRepo.java
+++ src/main/java/com/project/hospitalReport/repository/DrugInvoiceRepo.java
@@ -4,7 +4,11 @@ import com.project.hospitalReport.entity.DrugInvoice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface DrugInvoiceRepo extends JpaRepository<DrugInvoice, Long> {
+
+    Optional<DrugInvoice> findTopByOrderByIdDesc();
 
 }
